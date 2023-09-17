b3bc67c9d42bb0f8d533c98682cd5cf4624469e3
EternityRei <anastasiamayorova2003@gmail.com>
2023-09-17 20:57:30 +0300

    Add dtos

--- src/main/java/com/socialmedia/pub/dto/PublicationDTO.java
+++ src/main/java/com/socialmedia/pub/dto/PublicationDTO.java
@@ -1,4 +1,21 @@
 package com.socialmedia.pub.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@JsonTypeName("PUBLICATION")
 public class PublicationDTO {
+    private String id;
+    private String title;
+    private String content;
+    private UUID authorId;
+    private String authorUsername;
+    private Date creationDate;
+    private int likesCount;
 }
