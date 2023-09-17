a7de188d38be20d8e5059995ea29a8275ac66feb
EternityRei <anastasiamayorova2003@gmail.com>
2023-09-17 21:14:00 +0300

    Create repos for user and pubs entities

--- src/main/java/com/socialmedia/pub/repository/PublicationRepository.java
+++ src/main/java/com/socialmedia/pub/repository/PublicationRepository.java
@@ -1,4 +1,11 @@
 package com.socialmedia.pub.repository;
 
-public interface PublicationRepository {
+import com.socialmedia.pub.entity.Publication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PublicationRepository extends JpaRepository<Publication, UUID> {
 }
--- src/main/java/com/socialmedia/pub/repository/UserRepository.java
+++ src/main/java/com/socialmedia/pub/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.socialmedia.pub.repository;
 
-public interface UserRepository {
+import com.socialmedia.pub.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
