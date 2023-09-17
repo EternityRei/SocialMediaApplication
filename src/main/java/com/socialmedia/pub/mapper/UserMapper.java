b6dbffb406fd4a3f1261f18afb4d56b1235f1058
EternityRei <anastasiamayorova2003@gmail.com>
2023-09-17 21:08:17 +0300

    Add mappers

--- src/main/java/com/socialmedia/pub/mapper/UserMapper.java
+++ src/main/java/com/socialmedia/pub/mapper/UserMapper.java
@@ -1,4 +1,17 @@
 package com.socialmedia.pub.mapper;
 
+import com.socialmedia.pub.dto.UserDTO;
+import com.socialmedia.pub.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDTO userToUserDTO(User user);
+
+    User userDTOToUser(UserDTO userDTO);
 }
+
